0ce15dfa9fcd53638baf7f280d48853f7ed12ffd
FuLLeNN <62404655+FuLLeNN@users.noreply.github.com>
2022-05-01 20:14:45 +0200

    Add files via upload

--- handin7/dmaProg3/inversions/Inversions.java
+++ handin7/dmaProg3/inversions/Inversions.java
@@ -1,22 +1,16 @@
 package inversions;
 
-import java.io.*;
 import java.util.*;
 
-public class Inversions
-{
-  public int countInversions(ArrayList<Integer> input) {
-      int count = 0;
-      for(int i = 0; i<input.size();i++){
-          int j = i+1;
-          if(input.get(i)>input.get(j) && j<input.size()){
-              int inversion2 = input.get(j);
-              input.set(j,input.get(i));
-              input.set(i,inversion2);
-              count++;
-          }
-      }
-      return 42;
-  }
+public class Inversions {
+    public int countInversions(ArrayList<Integer> input) {
+        int count = 0; //to store inversion count
+        for (int i = 0; i < input.size() - 1; i++) //for each element
+            for (int j = i + 1; j < input.size(); j++) // to compare element right to current item
+                if (input.get(i) > input.get(j)) {
+                    count++;
+                }
+        return count;
+    }
 }
 
